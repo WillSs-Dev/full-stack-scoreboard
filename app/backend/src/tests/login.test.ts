8155eea7d6a647fc2455ad17d5d679b3e3887d20
Will <williansilva3dsi@hotmail.com>
2023-01-22 12:10:36 -0300

    Refactor: implementing mocks on a separete file

--- app/backend/src/tests/login.test.ts
+++ app/backend/src/tests/login.test.ts
@@ -5,22 +5,12 @@ import chaiHttp = require('chai-http');
 import { app } from '../app';
 import UserModel from '../database/models/User.model';
 import HTTPCodes from '../utils/HTTPCodes';
-
+import { adminUser, token } from './mocks/login.mock';
 import { test, describe } from 'mocha';
 
 chai.use(chaiHttp);
 
 const { expect } = chai;
-
-const adminUser = {
-  id: 1,
-  email: 'admin@admin.com',
-  password: '$2a$08$xi.Hxk1czAO0nZR..B393u10aED0RQ1N3PAEXQ7HxtLjKPEZBu.PW',
-  role: 'admin',
-};
-
-const token =
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImFkbWluQGFkbWluLmNvbSIsInBhc3N3b3JkIjoic2VjcmV0X2FkbWluIiwicm9sZSI6ImFkbWluIiwiaWF0IjoxNjc0MzM0ODQ3LCJleHAiOjE2NzQ5Mzk2NDd9.82c2yPpDBWQTjwaU6g_e3UT5XaDK4lUp-whgI8nCV-U';
 
 describe('Integration tests of login route', () => {
   afterEach(sinon.restore);
