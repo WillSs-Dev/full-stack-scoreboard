0c0eb8ccb71e7d739e9a1f61e41ca06754d3530c
Will <williansilva3dsi@hotmail.com>
2023-01-22 13:12:07 -0300

    Refactor: adding the team model stub on team's test

--- app/backend/src/tests/teams.test.ts
+++ app/backend/src/tests/teams.test.ts
@@ -5,6 +5,7 @@ import chaiHttp = require('chai-http');
 import { app } from '../app';
 import HTTPCodes from '../utils/HTTPCodes';
 import { teams } from './mocks/teams.mock';
+import TeamModel from '../database/models/Team.model';
 
 import { test, describe } from 'mocha';
 
@@ -15,6 +16,8 @@ const { expect } = chai;
 describe('Integration tests of the teams route', () => {
   afterEach(sinon.restore);
   test('The /teams route fetches all teams on the database', async () => {
+    sinon.stub(TeamModel, 'findAll').resolves(teams as TeamModel[]);
+
     const res = await chai.request(app).get('/teams');
 
     expect(res.status).to.be.equal(HTTPCodes.ok);
