d7b83021cf105c2f5ca47fd76e1ae385eae0a43f
Will <williansilva3dsi@hotmail.com>
2023-01-23 18:35:28 -0300

    Feat: testing the filtering of matches implementation

--- app/backend/src/tests/match.test.ts
+++ app/backend/src/tests/match.test.ts
@@ -5,7 +5,7 @@ import chaiHttp = require('chai-http');
 import { app } from '../app';
 import MatchModel from '../database/models/match.model';
 import HTTPCodes from '../utils/HTTPCodes';
-import { matches, matchesInProgress } from './mocks/match.mock';
+import { matches, matchesInProgress, pastMatches } from './mocks/match.mock';
 import { test, describe } from 'mocha';
 import IMatch from '../interfaces/Match';
 
@@ -15,7 +15,7 @@ const { expect } = chai;
 
 describe('Integration tests of match route', () => {
   afterEach(sinon.restore);
-  test('The /matches route fetches all teams on the database', async () => {
+  test('The /matches route fetches all matches on the database', async () => {
     sinon.stub(MatchModel, 'findAll').resolves(matches as IMatch[]);
 
     const res = await chai.request(app).get('/matches');
@@ -23,12 +23,20 @@ describe('Integration tests of match route', () => {
     expect(res.status).to.be.equal(HTTPCodes.ok);
     expect(res.body).to.be.deep.equal(matches);
   });
-  test("You can search the matches filtering by whether it's in progress or not", async () => {
-    sinon.stub(MatchModel, 'findAll').resolves(matches as IMatch[]);
+  test("You can search the matches that are in progress", async () => {
+    sinon.stub(MatchModel, 'findAll').resolves(matchesInProgress as IMatch[]);
 
     const res = await chai.request(app).get('/matches?inProgress=true');
 
     expect(res.status).to.be.equal(HTTPCodes.ok);
     expect(res.body).to.be.deep.equal(matchesInProgress);
+  });
+  test("You can search the past matches on the DB", async () => {
+    sinon.stub(MatchModel, 'findAll').resolves(pastMatches as IMatch[]);
+
+    const res = await chai.request(app).get('/matches?inProgress=false');
+
+    expect(res.status).to.be.equal(HTTPCodes.ok);
+    expect(res.body).to.be.deep.equal(pastMatches);
   });
 });
